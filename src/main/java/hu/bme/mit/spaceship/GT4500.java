351e3d8b51b7e3568548334345b38fdfb54bba4b
matesze41 <127397856+matesze41@users.noreply.github.com>
2024-04-12 13:44:05 +0200

    parsing error fix

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -83,8 +83,7 @@ public class GT4500 implements SpaceShip {
           firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
         }
         break;
-
+    }
     return firingSuccess;
   }
-
 }
