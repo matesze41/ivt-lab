321d9f1d1485c9ac337bd109f3a7308482a48e4e
matesze41 <127397856+matesze41@users.noreply.github.com>
2024-04-26 13:04:53 +0200

    'utolsosonarhiba'

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -27,7 +27,8 @@ public class TorpedoStore {
       }
     }
   }
-
+  private Random generator = new Random();
+  
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
@@ -36,7 +37,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
